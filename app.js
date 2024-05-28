7a24db95e318925da11c7068c2d4ee19016c5ce2
younus amshan <younusamshankhan@gmail.com>
2024-05-28 20:31:35 +0500

    new counter

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+var Counter = 0
+
+
+               //plusHandral function
+
+ plusHandral = () => {
+    Counter++
+    document.getElementById('Counter').innerHTML = Counter
+}
+
+                  //minusHandral function
+
+ minusHandarl = () => {
+    Counter--
+    document.getElementById('Counter').innerHTML = Counter
+}
+ 
+                   ////RestartHandral function                    
+
+ resttartHandral = () => {
+    Counter = 0
+    document.getElementById('Counter').innerHTML = Counter
+}
+    
